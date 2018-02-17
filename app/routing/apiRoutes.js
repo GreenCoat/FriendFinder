340bc4f89ab5c1fa71bae29245f2ad6000df2104
GreenCoat <nickscunningham@gmail.com>
2018-02-17 04:40:17 -0600

    Code mostly working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,39 @@
-var express = require('express');
-var router = express.Router();
+var friendData = require("../data/friends");
 
+module.exports = function(app){
+	app.get("/api/friends", function(req, res){
+		res.json(friendData);
+	});
 
+	app.post("/api/friends", function(req, res){
+		//recieve data
+		var data = req.body;
+		//Create var for holding difference
+		var highDif = 40;
+		var currentDif = 0;
+		var friend;
 
-module.exports = router;
+		//calculate friend data difference
+		for(var i = 0; i < friendData.length; i++){
+			currentDif = getDifference(data, friendData[i]);
+
+			if(currentDif < highDif){
+				highDif = currentDif;
+				friend = friendData[i];
+			}
+		}
+
+		friendData.push(data);
+
+		res.json(friend);
+	});
+
+	function getDifference(base, friend){
+		var dif = 0;
+
+		for(var i = 0; i < 10; i++){
+			dif += Math.abs(base["ans[]"][i] - friend.ans[i]);
+		}
+		return dif;
+	}
+}
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 //Dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
-var path = require ('path');
 
 //Set up express app
 var app = express();
@@ -11,7 +10,8 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(require('./app/routing/htmlRoutes.js'));
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
 
 app.listen(PORT, function(){
 	console.log("listening");
