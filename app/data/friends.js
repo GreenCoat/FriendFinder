d93ce97c6a6949aae7cdc3fc295e8b50b8008bb9
GreenCoat <nickscunningham@gmail.com>
2018-02-17 10:36:28 -0600

    Fixed weird bug kind of, need a better solution

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,14 +1,5 @@
 var friendData = [
-{
-	name: "Rem",
-	pic: "https://vignette.wikia.nocookie.net/rezero/images/0/02/Rem_Anime.png/revision/latest?cb=20160730213532",
-	ans: [1,1,1,1,1,1,1,1,1,1]
-},
-{
-	name: "Ram",
-	pic: "https://vignette.wikia.nocookie.net/rezero/images/e/ee/Ram_-_Re_Zero_Anime_BD_-_6.png/revision/latest?cb=20160914125706",
-	ans: [5,5,5,5,5,5,5,5,5,5]
-}
+
 ];
 
 module.exports = friendData;
